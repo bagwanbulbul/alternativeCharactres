f0302461e5bc5b68f2c465bc59ab5469b65c46d5
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-16 13:30:51 +0530

    try to solve with node js

--- altarnetiveArr.js
+++ altarnetiveArr.js
@@ -1,15 +1,54 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require("fs");
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 var raw_input = require('readline-sync').question
-function alternatingCharacters(s){
-    let result = 0;
-    let arr = s.split("");
+
+app.get('/get',function(req,res){
+    res.sendFile(path.join(__dirname+'/characters.html'));
+  });
+
+app.post("/post",function(req,res){
+    let result =0; 
+    let userInput = req.body.text
+    let arr = userInput.split("");
     for (var i=0; i<arr.length; i++){
-        if (arr[i]==arr[i+1]){
+        if(arr[i]==ar[i+1]){
             result = result+1
+            res.send(result)
         }
+
     }
-    console.log(result)
-    return result;
-}
-var str = raw_input("enter characters")
-alternatingCharacters(str)
+})
+
+
+
+app.listen(3000, () => console.log('server is listening 3000....'));
+
+
+
+
+
+
+
+
+
+
+
+
+// function alternatingCharacters(s){
+//     let result = 0;
+//     let arr = s.split("");
+//     for (var i=0; i<arr.length; i++){
+//         if (arr[i]==arr[i+1]){
+//             result = result+1
+//         }
+//     }
+//     console.log(result)
+//     return result;
+// }
+// var str = raw_input("enter characters")
+// alternatingCharacters(str)
 
---
+++ main.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>My First Web Page </title>
+            <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <script src="main.js"></script>
+        <input type="submit" value="Submit" id ="btn">
+    </body>
+</html>
